ff53d6c3a45fe72942aa0ed017f2a1d7e8a8591e
Andreas Rogers <a.rogers5@EDU.epsb.ca>
2022-02-15 14:38:29 -0700

    Update

--- Scripts/Max of Two Numbers.js
+++ Scripts/Max of Two Numbers.js
@@ -9,10 +9,24 @@ function main()
   //document.getElementById("CanBeAnything").innerHTML = alert ("You are Connected");
   document.getElementById("validityTest1").innerHTML = testNaN (firstNumber);
   document.getElementById("validityTest2").innerHTML = testNaN (secondNumber);
+  if (stop == true)
+  {
+    document.getElementById("answer").innerHTML = "Restart ... because I said so.";
+  } else
+  {
+    document.getElementById("answer").innerHTML = "What did you say?" + solution(firstNumber, secondNumber);
+  } //End stop=true
 }//End main()
 //
 function testNaN (number)
 {
+  if (number == "") //Checking for null, submit-button misfire
+  //Variable returns empty string, not null
+  {
+    stop = true;
+    return "Please type a number into the textfield";
+  }//End null check
+  //
   if ( isNaN(number) ) //Returns a Boolean
   {
     stop = true;
