530397f2e676fe5d212f96a4dbde1a915e4fffab
arogers25 <98920817+arogers25@users.noreply.github.com>
2022-02-16 13:53:59 -0700

    Update Max of Two Numbers.js

--- Scripts/Max of Two Numbers.js
+++ Scripts/Max of Two Numbers.js
@@ -22,6 +22,8 @@ function solution(number1, number2)
 //
 function main()
 {
+  stop = false; // Screen Refresh
+  
   let firstNumber = document.getElementById("textField1").value;
   let secondNumber = document.getElementById("textField2").value;
   //document.getElementById("CanBeAnything").innerHTML = alert ("You are Connected");
