009152fd58ba531c913ed6eb0f811b4626d2401d
arogers25 <98920817+arogers25@users.noreply.github.com>
2022-02-16 13:53:30 -0700

    Update Max of Two Numbers.js

--- Scripts/Max of Two Numbers.js
+++ Scripts/Max of Two Numbers.js
@@ -1,7 +1,25 @@
 console.log("You made it to the Max of Two Numbers.js!");
-
+//
+//Global Variables
 let stop = false;
-
+//
+function solution(number1, number2)
+{
+  if ( number1 == number2 ) return "equal";
+  //Ternary Operator Example (single comparison), needs single line IF to limit choices
+  return ( number1 > number2 ) ? "Number 1 is larger" : "Number 2 is larger";
+  //
+  /* Repeats the ternary operator
+  if ( number1 > number2 )
+  {
+    return "Number 1 is larger";
+  } else
+  {
+    return "Number 2 is larger";
+  }
+  */
+}//End solution
+//
 function main()
 {
   let firstNumber = document.getElementById("textField1").value;
@@ -14,7 +32,7 @@ function main()
     document.getElementById("answer").innerHTML = "Restart ... because I said so.";
   } else
   {
-    document.getElementById("answer").innerHTML = "What did you say?" + solution(firstNumber, secondNumber);
+    document.getElementById("answer").innerHTML = "What did you say? " + solution(firstNumber, secondNumber);
   } //End stop=true
 }//End main()
 //
