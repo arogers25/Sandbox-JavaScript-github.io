efb598498e0d680068618108b5a7f25df99ae041
arogers25 <98920817+arogers25@users.noreply.github.com>
2022-02-22 14:39:50 -0700

    Update

--- Scripts/Landscape or Portrait.js
+++ Scripts/Landscape or Portrait.js
@@ -5,32 +5,47 @@ let stop = false; //Boolean for testNaN() validation function, see end of file
 function javascript () { //Where colsole.log tested file goes
 
 }
+function testNaN (number)
+{
+  if (number == "") //Checking for null, submit-button misfire
+  //Variable returns empty string, not null
+  {
+    stop = true;
+    return "Please type a number into the textfield";
+  }//End null check
+  //
+  if ( isNaN(number) ) //Returns a Boolean
+  {
+    stop = true;
+    return "Type a Real Number";
+  } else
+  {
+    return "Input Validated";
+  }
+}//end testNaN
+//
+function geometry (w, h) // width and height parameters
+{
+  if (w == h) return "Square";
+  return (w > h) ? "Landscape" : "Portrait";
+}//end geometry
+//
 //Main Function, necessary lines of code
 function main() {
-  let firstNumber, secondNumber; //Variables particular to JavsScript Assignment
+  stop = false
+  let width, height; //Variables particular to JavsScript Assignment
   //Get the value of the Heighth Input Field, id="testfield1", assign it to a variable
-  firstNumber = document.getElementById("textField1").value;
-  secondNumber = document.getElementById("textField2").value;
-  //Validate by alert or HTML Text in p-tag
-  document.getElementById("validityTest1").innerHTML = alert (testNAN (firstNumber) );
-  document.getElementById("validityTest1").innerHTML = testNAN (firstNumber);
-  document.getElementById("validityTest2").innerHTML = alert (testNAN (secondNumber) );
-  document.getElementById("validityTest2").innerHTML = testNAN (secondNumber);
+  width = document.getElementById("textField1").value;
+  height = document.getElementById("textField2").value;
+  document.getElementById("validityTest1").innerHTML = testNaN(width);
+  document.getElementById("validityTest2").innerHTML = testNaN(height);
   if (stop == true) {
-      document.getElementById("answer").innerHTML = "Restart because I said so." //Change to more appropriate message
+      document.getElementById("answer").innerHTML = "Unknown" //Change to more appropriate message
+      alert("Please re-enter your values");
     } else {
       //Difference between calling functions with arguements and sending to parameter's, local variables
-      console.log("What did you say?", javaScriptFunction-Solution (firstNumber, secondNumber)); // Change to more appropriate message
-      document.getElementById("answer").innerHTML = "What did you say? " + geometry (firstNumber, secondNumber);
+      //console.log("What did you say?", javaScriptFunction-Solution (width, height)); // Change to more appropriate message
+      document.getElementById("answer").innerHTML = geometry (width, height);
     }
 } //End main()
 //
-function testNaN (number) {
-  if ( isNaN (number) ) { //NaN are not values o REAL Number System
-    stop = true;
-    return "Type a Real Number"
-  } else {
-    return "Input Validated"
-  }
-
-}//End testNaN()
--- Scripts/Modulus Assignment.js
+++ Scripts/Modulus Assignment.js
@@ -1,36 +1,46 @@
-console.log("You made it to Modules Assignments.js");
+console.log("You made it to Modulus Assignment.js");
 
 let stop = false; //Boolean for testNaN() validation function, see end of file
 
 function javascript () { //Where colsole.log tested file goes
 
 }
+//
+function testNaN (number)
+{
+  if (number == "") //Checking for null, submit-button misfire
+  //Variable returns empty string, not null
+  {
+    stop = true;
+    return "Please type a number into the textfield";
+  }//End null check
+  //
+  if ( isNaN(number) ) //Returns a Boolean
+  {
+    stop = true;
+    return "Type a Real Number";
+  } else
+  {
+    return "Input Validated";
+  }
+}//end testNaN
+//
+function evenOrOdd (number)
+{
+  return (number%2 == 0) ? "Even" : "Odd"
+}//end evenOrOdd
+//
 //Main Function, necessary lines of code
 function main() {
-  let firstNumber, secondNumber; //Variables particular to JavsScript Assignment
-  //Get the value of the Heighth Input Field, id="testfield1", assign it to a variable
+  stop = false;
+  let firstNumber; //Variables particular to JavsScript Assignment
   firstNumber = document.getElementById("textField1").value;
-  secondNumber = document.getElementById("textField2").value;
-  //Validate by alert or HTML Text in p-tag
-  document.getElementById("validityTest1").innerHTML = alert (testNAN (firstNumber) );
-  document.getElementById("validityTest1").innerHTML = testNAN (firstNumber);
-  document.getElementById("validityTest2").innerHTML = alert (testNAN (secondNumber) );
-  document.getElementById("validityTest2").innerHTML = testNAN (secondNumber);
+  testNaN(firstNumber);
   if (stop == true) {
-      document.getElementById("answer").innerHTML = "Restart because I said so." //Change to more appropriate message
+      alert(testNaN(firstNumber));
+      document.getElementById("answer").innerHTML = "Unknown" //Change to more appropriate message
     } else {
       //Difference between calling functions with arguements and sending to parameter's, local variables
-      console.log("What did you say?", javaScriptFunction-Solution (firstNumber, secondNumber)); // Change to more appropriate message
-      document.getElementById("answer").innerHTML = "What did you say? " + geometry (firstNumber, secondNumber);
+      document.getElementById("answer").innerHTML = evenOrOdd(firstNumber);
     }
 } //End main()
-//
-function testNaN (number) {
-  if ( isNaN (number) ) { //NaN are not values o REAL Number System
-    stop = true;
-    return "Type a Real Number"
-  } else {
-    return "Input Validated"
-  }
-
-}//End testNaN()
