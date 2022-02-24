63bbb4ad15d092c476d3ed79acc3d4ad15b77999
arogers25 <98920817+arogers25@users.noreply.github.com>
2022-02-24 14:19:09 -0700

    Update

--- Scripts/FizzBuzz.js
+++ Scripts/FizzBuzz.js
@@ -5,32 +5,43 @@ let stop = false; //Boolean for testNaN() validation function, see end of file
 function javascript () { //Where colsole.log tested file goes
 
 }
+//
+function testNaN (number)
+{
+  if (number == "") //Checking for null, submit-button misfire
+  //Variable returns empty string, not null
+  {
+    stop = true;
+    return "Please type a number into the textfield";
+  }//End null check
+  //
+  if ( isNaN(number) ) //Returns a Boolean
+  {
+    stop = true;
+    return "Type a Real Number";
+  } else
+  {
+    return "Input Validated";
+  }
+}//end testNaN
+//
+function fizzBuzz(number)
+{
+  let out = ""; // The output for our FizzBuzz return
+  if (number % 3 == 0) out += "Fizz";
+  if (number % 5 == 0) out += "Buzz";
+  return (out == "") ? number : out; //If nothing was added to the output it is not divisible by 3 or 5
+}
 //Main Function, necessary lines of code
 function main() {
-  let firstNumber, secondNumber; //Variables particular to JavsScript Assignment
-  //Get the value of the Heighth Input Field, id="testfield1", assign it to a variable
-  firstNumber = document.getElementById("textField1").value;
-  secondNumber = document.getElementById("textField2").value;
-  //Validate by alert or HTML Text in p-tag
-  document.getElementById("validityTest1").innerHTML = alert (testNAN (firstNumber) );
-  document.getElementById("validityTest1").innerHTML = testNAN (firstNumber);
-  document.getElementById("validityTest2").innerHTML = alert (testNAN (secondNumber) );
-  document.getElementById("validityTest2").innerHTML = testNAN (secondNumber);
+  stop = false;
+  let firstNumber; //Variables particular to JavsScript Assignment
+  firstNumber = document.getElementById("textField1").value
+  let validCheck = testNaN (firstNumber); // Test the number once, save it, and have it appear as an alert instead of putting it in a validity check
   if (stop == true) {
-      document.getElementById("answer").innerHTML = "Restart because I said so." //Change to more appropriate message
+      alert(validCheck);
+      document.getElementById("answer").innerHTML = "Unknown";
     } else {
-      //Difference between calling functions with arguements and sending to parameter's, local variables
-      console.log("What did you say?", javaScriptFunction-Solution (firstNumber, secondNumber)); // Change to more appropriate message
-      document.getElementById("answer").innerHTML = "What did you say? " + geometry (firstNumber, secondNumber);
+      document.getElementById("answer").innerHTML = fizzBuzz(firstNumber);
     }
 } //End main()
-//
-function testNaN (number) {
-  if ( isNaN (number) ) { //NaN are not values o REAL Number System
-    stop = true;
-    return "Type a Real Number"
-  } else {
-    return "Input Validated"
-  }
-
-}//End testNaN()
